5dfeeb873cbf9f7f3bc5d571a7a56c862f1a9d99
ianw3214 <ianw3214@gmail.com>
2022-09-26 22:31:34 -0700

    Remove SDL2 ttf

--- src/engine/engine.rs
+++ src/engine/engine.rs
@@ -2,7 +2,6 @@ use sdl2::EventPump;
 use sdl2::render::{TextureCreator, WindowCanvas};
 use sdl2::video::WindowContext;
 use sdl2::image::{self, InitFlag};
-use sdl2::ttf::Sdl2TtfContext;
 
 use std::time::SystemTime;
 
@@ -10,8 +9,6 @@ pub struct Engine {
     pub canvas : WindowCanvas,
     pub event_pump : EventPump,
     pub texture_creator : TextureCreator<WindowContext>,
-
-    pub ttf_context : Sdl2TtfContext,
 
     pub last_update : SystemTime
 }
@@ -32,15 +29,10 @@ pub fn init_engine() -> Engine {
     let event_pump = sdl_context.event_pump()
         .expect("could not create event pump");
 
-    
-    // text rendering
-    let ttf_context = sdl2::ttf::init().expect("Could not initialize ttf context");
-
     Engine {
         canvas : canvas,
         event_pump : event_pump,
         texture_creator : texture_creator,
-        ttf_context : ttf_context,
         last_update : SystemTime::now()
     }
 }
